f75657f524e703298381c051e3b32950ed2e2e2f
Prince <luizfas@dcc.ufrj.br>
2022-08-01 10:01:23 -0300

    Alterando .env

--- backend/src/app/config/database-config.js
+++ backend/src/app/config/database-config.js
@@ -3,11 +3,11 @@ const env = process.env;
 
 const config = {
     db: { /* do not put password or any sensitive info here, done only for demo */
-        host: 'ec2-34-235-198-25.compute-1.amazonaws.com',
-        port: '5432',
-        user: 'tdvfqbrapdwzdf',
-        password:  'bee5da3b305d3418ac43a3fb064d366185f259c159071ebf997efb9f804a2249',
-        database: 'd6434rhm51jru8',
+        host: env.DBHOST || 'ec2-34-235-198-25.compute-1.amazonaws.com',
+        port: env.DBPORT || 5432,
+        user: env.DBUSER || 'tdvfqbrapdwzdf',
+        password:  env.DBPASSWORD || 'bee5da3b305d3418ac43a3fb064d366185f259c159071ebf997efb9f804a2249',
+        database: env.DBNAME || 'd6434rhm51jru8',
         ssl: {
             rejectUnauthorized: false
         }
